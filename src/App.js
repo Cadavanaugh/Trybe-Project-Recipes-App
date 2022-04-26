bffccfded721e4746419b5261d6af4f82dcb7114
Queite <queitesc@gmail.com>
2022-04-26 18:45:47 -0300

    :ok_hand:style: correções lint

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Switch } from 'react-router-dom';
+// import { Switch } from 'react-router-dom';
 
 function App() {
   return (
--- src/Context/RecipesProvider.jsx
+++ src/Context/RecipesProvider.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import RecipesContext from './RecipesContext';
 
 function RecipesProvider({ children }) {
   return (
-      <RecipesContext.Provider value={ {algo} } >
-        { children }
-      </RecipesContext.Provider>
+    <RecipesContext.Provider value={ algo }>
+      { children }
+    </RecipesContext.Provider>
   );
 }
+
+RecipesProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default RecipesProvider;
