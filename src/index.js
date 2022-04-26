3007dac3a4f3309433c67d2735ce1b09537f6da2
Queite <queitesc@gmail.com>
2022-04-26 18:59:05 -0300

    :bug:fix: inserindo providers em index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import LoginProvider from './Context/LoginProvider';
+import RecipesProvider from './Context/RecipesProvider';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <LoginProvider>
+        <RecipesProvider>
+          <App />
+        </RecipesProvider>
+      </LoginProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
