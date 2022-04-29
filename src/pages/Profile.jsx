43c2034bc5efaae333f6c183fa3ebc749dfc4671
Daniel <daniel.figueiredo1994@gmail.com>
2022-04-28 22:35:08 -0300

    :heavy_check_mark: feat: Profile feito por completo

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const history = useHistory();
+
+  const handleClickProfle = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
+  const um = -1;
   return (
-    <div>
-      Profile
+    <>
+      <Header pageTitle="Profile" />
+      <div>
+        <h2
+          data-testid="profile-email"
+        >
+          {(localStorage.getItem('user')).split('"').slice(1, um).pop('l')}
+        </h2>
+        <button
+          data-testid="profile-done-btn"
+          type="submit"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          type="submit"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          type="submit"
+          onClick={ () => handleClickProfle() }
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
-    </div>
+    </>
   );
 }
 
