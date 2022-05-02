5fe05bfdfe9992ec087a88f1dc9a638074c9d35b
Queite <queitesc@gmail.com>
2022-05-02 16:14:51 -0300

    🐛 fix: Bug req 32 causado pela lista de ingredientes resolvido

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -11,8 +11,12 @@ export default function Ingredients({ recipe }) {
     .filter((item) => item[0].includes('strIngredient') && item[1] !== emptyIngredient);
   const measuresList = Object.entries(recipe[0])
     .filter((item) => item[0].includes('strMeasure') && item[1] !== emptyIngredient2);
+  console.log(measuresList);
   // Aqui causa bug na 32 pq há ingredientes sem medidas em bebidas
   const renderIngredients = ingredientsList.reduce((acc, cur, idx) => {
+    while (ingredientsList.length >= measuresList.length) {
+      measuresList.push(['', '']);
+    }
     acc = [...acc, [cur[1], measuresList[idx][1]]];
     return acc;
   }, []);
