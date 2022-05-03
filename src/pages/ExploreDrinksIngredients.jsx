de39f8fd088c636525aca34002fbb23064288595
romulo-rp29 <romulo.ceuma@gmail.com>
2022-05-02 23:47:37 -0300

    :construction: feat: cards de ingredientes na telas de explorar ingredientes

--- src/pages/ExploreDrinksIngredients.jsx
+++ src/pages/ExploreDrinksIngredients.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import { Card } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 function ExploreDrinksIngredients() {
+  const [info, setInfo] = useState({});
+  //
+  // endpoints:
+  //
+  // ingredientes: https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list
+  // imagens dos ingredientes: www.thecocktaildb.com/images/ingredients/{nome do ingrediente}-Small.png
+  //
+  useEffect(() => {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+      .then((response) => response.json())
+      .then((response) => {
+        setInfo(response);
+        console.log(response);
+      });
+  }, []);
+
+  const maxIngToShow = 12;
+  const imgAPI = 'https://www.thecocktaildb.com/images/ingredients';
   return (
-    <div>
-      oi
-    </div>
+    <>
+      <Header pageTitle="Explore Ingredients" showSearchButton />
+      {info.drinks && (
+        <div>
+          {info.drinks.map((ingredient, index) => (
+            index < maxIngToShow && (
+              <div
+                key={ index }
+                data-testid={ `${index}-ingredient-card` }
+              >
+                {/* <Link exact="true" to={ path }> */}
+                <img
+                  src={ `${imgAPI}/${ingredient.strIngredient1}-Small.png` }
+                  alt={ ingredient.strIngredient1 }
+                  width="100px"
+                  data-testid={ `${index}-card-img` }
+                />
+                {/* </Link> */}
+                <p
+                  data-testid={ `${index}-card-name` }
+                >
+                  {ingredient.strIngredient1}
+
+                </p>
+              </div>
+            )
+          ))}
+        </div>
+      )}
+    </>
   );
 }
 
--- src/pages/ExploreFoodsIngredients.jsx
+++ src/pages/ExploreFoodsIngredients.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react';
+// import { Card } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 
 function ExploreFoodsIngredients() {
   const [info, setInfo] = useState({});
@@ -8,6 +11,7 @@ function ExploreFoodsIngredients() {
   // categorias: https://www.themealdb.com/api/json/v1/1/list.php?c=list
   // nacionalidades: https://www.themealdb.com/api/json/v1/1/list.php?a=list
   // ingredientes: https://www.themealdb.com/api/json/v1/1/list.php?i=list
+  // imagens dos ingredientes: https://www.themealdb.com/images/ingredients/{nome-do-ingrediente}-Small.png
   //
   useEffect(() => {
     fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list')
@@ -17,19 +21,40 @@ function ExploreFoodsIngredients() {
         console.log(response);
       });
   }, []);
-  const maxShowed = 12;
+
+  const maxIngToShow = 12;
+  const imgAPI = 'https://www.themealdb.com/images/ingredients';
   return (
-    <div>
+    <>
+      <Header pageTitle="Explore Ingredients" showSearchButton />
       {info.meals && (
-        <ul>
+        <div>
           {info.meals.map((ingredient, index) => (
-            <li key={ ingredient.idIngredient }>
-              {index < maxShowed && ingredient.strIngredient}
-            </li>
+            index < maxIngToShow && (
+              <div
+                key={ index }
+                data-testid={ `${index}-ingredient-card` }
+              >
+                {/* <Link exact="true" to={ path }> */}
+                <img
+                  src={ `${imgAPI}/${ingredient.strIngredient}-Small.png` }
+                  alt={ ingredient.strIngredient }
+                  width="100px"
+                  data-testid={ `${index}-card-img` }
+                />
+                {/* </Link> */}
+                <p
+                  data-testid={ `${index}-card-name` }
+                >
+                  {ingredient.strIngredient}
+
+                </p>
+              </div>
+            )
           ))}
-        </ul>
+        </div>
       )}
-    </div>
+    </>
   );
 }
 
