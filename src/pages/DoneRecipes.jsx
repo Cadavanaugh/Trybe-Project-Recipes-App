5acebd03a2078d4a993d549d0b41c57072c8367c
Jovane Castro <jovanecastro12@gmail.com>
2022-05-05 00:16:01 -0300

    :bug: fix: Arrumando requisito 10

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -2,26 +2,14 @@ import React, { useState } from 'react';
 import DoneCard from '../components/DoneCard';
 import Header from '../components/Header';
 
-// [{
-//     id: id-da-receita,
-//     type: comida-ou-bebida,
-//     nationality: nacionalidade-da-receita-ou-texto-vazio,
-//     category: categoria-da-receita-ou-texto-vazio,
-//     alcoholicOrNot: alcoholic-ou-non-alcoholic-ou-texto-vazio,
-//     name: nome-da-receita,
-//     image: imagem-da-receita,
-//     doneDate: quando-a-receita-foi-concluida,
-//     tags: array-de-tags-da-receita-ou-array-vazio
-// }]
-
-function DoneRecipes() {
+export default function DoneRecipes() {
   if (!localStorage.doneRecipes) localStorage.doneRecipes = '[]';
   const localStored = JSON.parse(localStorage.doneRecipes);
   const [done, setDone] = useState(localStored);
 
   return (
     <>
-      <Header pageTitle="Favorite Recipes" showSearchButton />
+      <Header pageTitle="Done Recipes" showSearchButton />
       <div>
         <button
           data-testid="filter-by-all-btn"
@@ -65,5 +53,3 @@ function DoneRecipes() {
     </>
   );
 }
-
-export default DoneRecipes;
