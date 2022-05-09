e58d299052e8827766b4e2ecd659670fae38e82a
Queite <queitesc@gmail.com>
2022-05-09 20:53:33 -0300

    :heavy_check_mark: feat: 78 a 80 ok

--- src/pages/ExploreNationalities.jsx
+++ src/pages/ExploreNationalities.jsx
@@ -1,53 +1,58 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import { fetchNationalities } from '../services/fetchCategories';
 import RecipesContext from '../context/RecipesContext';
 import Card from '../components/Card';
+import { doze } from '../services/variables';
+import { fetchFoodsByNationality } from '../services/fetchFoodsAndDrinks';
+// import { fetchNationalities } from '../services/fetchCategories';
 
 function Nationalities() {
-  // const { meals, foodCategories, exploreMeals,
-  //   ingredientFood, setIngredientFood } = useContext(RecipesContext);
-  const { setError } = useContext(RecipesContext);
-  const [nationalities, setNationalities] = useState();
+  const { meals } = useContext(RecipesContext);
   const [loading, setLoading] = useState(false);
-
-  // const [categorizedMeals, setCategorizedMeals] = useState([]);
-  // const [isFilter, setIsFilter] = useState(false);
-  // const [renderCards, setRenderCards] = useState([]);
+  const [renderCards, setRenderCards] = useState([]);
+  const [nationalities, setNationalities] = useState([]);
+  const [mealsByNationality, setMealsByNationality] = useState([]);
+  const [isFilter, setIsFilter] = useState(false);
+  console.log(mealsByNationality);
   // const [category, setCategory] = useState('All');
 
-  useEffect(() => {
-    fetchNationalities(setNationalities, setError);
-  });
-
-  const handleSelect = async (choosenCategory) => {
-    setIngredientFood([]);
-    if (choosenCategory === category || choosenCategory === 'All') {
+  const handleSelect = async ({ target }) => {
+    console.log(target.value);
+    if (target.value === 'All') {
       setIsFilter(false);
+      setRenderCards(meals);
     } else {
       setLoading(true);
-      const filteredFoods = await fetchFoodsByCategory(choosenCategory);
-      setCategorizedMeals(filteredFoods);
+      const filteredFoods = await fetchFoodsByNationality(target.value); // fazer o fetch desta
+      setMealsByNationality(filteredFoods);
       setIsFilter(true);
-      setCategory(choosenCategory);
       setLoading(false);
     }
   };
 
-  // Usado para decidir o que deve ser renderizado
   // useEffect(() => {
-  //   if (ingredientFood.length) {
-  //     setRenderCards(ingredientFood);
-  //   } else if (isFilter) {
-  //     setRenderCards(categorizedMeals);
-  //   } else if (location.explore) {
-  //     setRenderCards(exploreMeals);
-  //   } else {
-  //     setRenderCards(meals);
-  //   }
-  // }, [meals, ingredientFood, exploreMeals, isFilter, categorizedMeals]);
-  // console.log(nationalities);
+  //   fetchNationalities(setNationalities, setError);
+  // }, []);
+
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/list.php?a=list')
+      .then((response) => response.json())
+      .then((response) => {
+        setNationalities([{ strArea: 'All' }, ...response.meals]);
+        console.log(response);
+      });
+  }, []);
+
+  // Usado para decidir o que deve ser renderizado
+  useEffect(() => {
+    if (!isFilter) {
+      setRenderCards(meals);
+    } else if (mealsByNationality) {
+      setRenderCards(mealsByNationality);
+    }
+  }, [meals, isFilter, mealsByNationality]);
+  console.log(nationalities);
 
   return (
     <div>
@@ -61,6 +66,7 @@ function Nationalities() {
           <option
             key={ index }
             data-testid={ `${nationality.strArea}-option` }
+            value={ nationality.strArea }
           >
             {nationality.strArea}
           </option>
