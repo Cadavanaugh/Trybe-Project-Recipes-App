bd1bbfb61810429cdd3775c3f4bedf7a1f321599
Queite <queitesc@gmail.com>
2022-05-09 16:55:04 -0300

    :lipstick: style: add remy

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -3,33 +3,31 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import PageButton from '../components/PageButton';
 import style from '../styles/Explore.module.css';
-// import remy from '../images/remy.png';
+import remy from '../images/remy.png';
 
 function Explore() {
   return (
-    <>
+    <div className={ style['explore-container'] }>
       <Header
         pageTitle="Explore"
         showSearchButton
         className={ style.header }
       />
-      <div className={ style['explore-container'] }>
-        <div className={ style['button-container'] }>
-          <PageButton
-            targetPageTitle="Explore Foods"
-            dataTestId="explore-foods"
-            routePath="/explore/foods"
-          />
-          <PageButton
-            targetPageTitle="Explore Drinks"
-            dataTestId="explore-drinks"
-            routePath="/explore/drinks"
-          />
-        </div>
-        {/* <img src={ remy } alt="Remy with a spoon" /> */}
-        <Footer />
+      <div className={ style['button-container'] }>
+        <PageButton
+          targetPageTitle="Explore Foods"
+          dataTestId="explore-foods"
+          routePath="/explore/foods"
+        />
+        <PageButton
+          targetPageTitle="Explore Drinks"
+          dataTestId="explore-drinks"
+          routePath="/explore/drinks"
+        />
       </div>
-    </>
+      <img src={ remy } alt="Remy with a spoon" className={ style.remy } />
+      <Footer />
+    </div>
   );
 }
 
